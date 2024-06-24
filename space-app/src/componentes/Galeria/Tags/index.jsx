22fb285c882925b1ec33b4001b1863dd66821ba8
William Santana <williamandrade1058@gmail.com>
2024-06-24 14:44:26 -0300

    adicionando tags

--- space-app/src/componentes/Galeria/Tags/index.jsx
+++ space-app/src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,48 @@
+import styled from 'styled-components';
+import tags from './tags.json';
+
+const TagsContainer = styled.section`
+    display: flex;
+    align-items: center;
+`;
+const TituloTag = styled.h3`
+    color: #D9D9D9;
+`;
+
+const ContainerButtons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    padding: 12px;
+`;
+
+const ButtonTags = styled.button`
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #C98CF1;
+    }
+`;
+
+
 const Tags = () => {
-    return <h1>Tags</h1>
+    return <>
+        <TagsContainer>
+            <TituloTag>Busque por tags:</TituloTag>
+            <ContainerButtons
+            >
+                {tags.map(tag => <ButtonTags key={tag.id}>{tag.titulo}</ButtonTags>)}
+            </ContainerButtons>
+        </TagsContainer>
+    </>
 }
 
 export default Tags;
